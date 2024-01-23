d58fbd8844239f2bfe2f7ad7adeaa4f908c457c4
unplaced <eduardo.dev.ads@gmail.com>
2024-01-22 22:16:13 -0300

    This is the initial commit about the Cafe Managemente System Project

---
+++ src/main/java/com/inn/cafesystem/CafesystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.inn.cafesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafesystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafesystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inn/cafesystem/CafesystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.cafesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafesystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
